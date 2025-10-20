9ba8e28dc09fe1da8939edf393c935213d1e6633
fengitfl <enikonov229@gmail.com>
2025-10-20 20:01:29 +0300

    Add files via upload

--- src/lab_5/csv_xlsx.py
+++ src/lab_5/csv_xlsx.py
@@ -1,19 +1,31 @@
-import os,sys, csv
+import os, csv, sys
+
 from openpyxl import Workbook
-wb = Workbook()
-ws  = wb.active
-ws.title  = "Sheet1"
+
 def csv_to_xlsx(csv_path: str, xlsx_path: str) -> None:
     if not os.path.exists(csv_path):
         print("FileNotFoundError")
         sys.exit(1)
+
     if os.path.getsize(csv_path) == 0:
         print("ValueError")
         sys.exit(1)
-    with open(csv_path ,"r", encoding="utf-8") as csv_file:
-        for row in csv.reader(csv_file):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Sheet1"
+
+    with open(csv_path, "r", encoding="utf-8") as csv_file:
+        reader = csv.reader(csv_file)
+        for row in reader:
             ws.append(row)
-        wb.save(xlsx_path)
+    for column_cells in ws.columns:
+        max_length = 0
+        column_letter = column_cells[0].column_letter
+        for cell in column_cells:
+            if cell.value:
+                max_length = max(max_length, len(str(cell.value)))
+        ws.column_dimensions[column_letter].width = max(max_length + 2, 8)
+    wb.save(xlsx_path)
 csv_to_xlsx(r"C:\Users\eniko\PycharmProjects\PythonProject\data\samples\cities.csv", r"C:\Users\eniko\PycharmProjects\PythonProject\data\out\cities.xlsx")
 
 
