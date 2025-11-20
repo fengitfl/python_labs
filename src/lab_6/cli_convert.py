9080aafef0d5415f27e3d252ab64c669adc518e8
fengitfl <enikonov229@gmail.com>
2025-11-20 10:06:38 +0300

    --

--- src/lab_6/cli_convert.py
+++ src/lab_6/cli_convert.py
@@ -2,66 +2,26 @@ import sys, argparse
 
 sys.path.append(r"C:\Users\eniko\Gitrep\python_labs\src\lib")
 sys.path.append(r"C:\Users\eniko\Gitrep\python_labs\src\lab_6")
-from csv_xlsx import (
-    csv_to_xlsx,
-)
-from json_csv import (
-    json_to_csv,
-    csv_to_json,
-)
-from cli_text import (
-    check_file,
-)
+from csv_xlsx import csv_to_xlsx
+from json_csv import json_to_csv, csv_to_json
+from cli_text import check_file
 
 
 def cli_convert():
     parser = argparse.ArgumentParser(description="Конвертеры данных")
-    sub = parser.add_subparsers(
-        dest="cmd",
-        required=True,
-    )
+    sub = parser.add_subparsers(dest="cmd", required=True)
 
     p1 = sub.add_parser("json2csv")
-    p1.add_argument(
-        "--in",
-        dest="input",
-        required=True,
-        help="Входной JSON файл",
-    )
-    p1.add_argument(
-        "--out",
-        dest="output",
-        required=True,
-        help="Выходной CSV файл",
-    )
+    p1.add_argument("--in", dest="input", required=True, help="Входной JSON файл")
+    p1.add_argument("--out", dest="output", required=True, help="Выходной CSV файл")
 
     p2 = sub.add_parser("csv2json")
-    p2.add_argument(
-        "--in",
-        dest="input",
-        required=True,
-        help="Входной CSV файл",
-    )
-    p2.add_argument(
-        "--out",
-        dest="output",
-        required=True,
-        help="Выходной JSON файл",
-    )
+    p2.add_argument("--in", dest="input", required=True, help="Входной CSV файл")
+    p2.add_argument("--out", dest="output", required=True, help="Выходной JSON файл")
 
     p3 = sub.add_parser("csv2xlsx")
-    p3.add_argument(
-        "--in",
-        dest="input",
-        required=True,
-        help="Входной CSV файл",
-    )
-    p3.add_argument(
-        "--out",
-        dest="output",
-        required=True,
-        help="Выходной XLSX файл",
-    )
+    p3.add_argument("--in", dest="input", required=True, help="Входной CSV файл")
+    p3.add_argument("--out", dest="output", required=True, help="Выходной XLSX файл")
 
     args = parser.parse_args()
 
@@ -71,10 +31,7 @@ def cli_convert():
                 print(f"Ошибка: Файл {args.input} не существует или недоступен")
                 sys.exit(1)
 
-            json_to_csv(
-                args.input,
-                args.output,
-            )
+            json_to_csv(args.input, args.output)
             print(f"Успешно: JSON -> CSV")
 
         elif args.cmd == "csv2json":
@@ -82,10 +39,7 @@ def cli_convert():
                 print(f"Ошибка: Файл {args.input} не существует или недоступен")
                 sys.exit(1)
 
-            csv_to_json(
-                args.input,
-                args.output,
-            )
+            csv_to_json(args.input, args.output)
             print(f"Успешно: CSV -> JSON")
 
         elif args.cmd == "csv2xlsx":
@@ -93,10 +47,7 @@ def cli_convert():
                 print(f"Ошибка: Файл {args.input} не существует или недоступен")
                 sys.exit(1)
 
-            csv_to_xlsx(
-                args.input,
-                args.output,
-            )
+            csv_to_xlsx(args.input,args.output)
             print(f"Успешно: CSV -> XLSX")
 
         else:
@@ -113,4 +64,4 @@ def cli_convert():
 if __name__ == "__main__":
     sys.exit(cli_convert())
 
-# python src\lab_6\cli_convert.py json2csv --in data\samples\people.json --out data\out\people_from_json.csv
+
